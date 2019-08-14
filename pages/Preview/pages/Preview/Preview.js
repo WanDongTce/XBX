dc91bb13a4c52d9e1c54fc60f3346a551cd26170
qq22711289 <weixiangyingln@163.com>
2019-08-14 14:46:04 +0800

    学霸学

--- pages/Preview/pages/Preview/Preview.js
+++ pages/Preview/pages/Preview/Preview.js
@@ -39,234 +39,234 @@ Page({
 
 
 
-  },
-  topshoop: function () {
-    wx.navigateBack({
-      delta: 1
-    })
-  },
-  getdata:function(){
-    var that=this
-    wx.request({
-      url: 'http://yuxile.54xuebaxue.com/index/Typepublic/tList',
-      header: {
-        'content-type': 'application/x-www-form-urlencoded'
-      },
-      method: 'POST',
-      success:function(res){
-        
-        var list_wei=[]
-        
-        console.log(res.data.data)
-        //科目ID
-        courseid = res.data.data.course_list[0].id
-        //版本ID
-        versionid = res.data.data.version_list[0].id
-        //年级id
-        gradeid = res.data.data.grade_list[0].id
-        that.getteaching()
-        console.log(versionid)
-        nianji=res.data.data.grade_list[0].name
-        banben = res.data.data.version_list[0].name
-        that.setData({
-          class_list: res.data.data.grade_list,
-          version_list: res.data.data.version_list,
-          Subject_list: res.data.data.course_list,
-          nianji: res.data.data.grade_list[0].name,
-          banben: res.data.data.version_list[0].name
-          
+    },
+    topshoop: function () {
+        wx.navigateBack({
+            delta: 1
         })
-        
-      }
-    })
-  },
-  tobooklist:function(e){
-    var postad = e.currentTarget.dataset.id 
-    //教材名称
-    wx.setStorageSync("jcmc", e.currentTarget.dataset.name)
-    console.log(postad)
-    wx.navigateTo({
-      //url: 'post-detail/post-detail'  //跳转详情页  切记配置app.json文件 
-      url: '../booklist/booklist?id=' + postad + "&nianji=" + nianji + "&banben=" + banben  //传递参数
-    })
+    },
+    getdata:function(){
+        var that=this
+        wx.request({
+            url: 'http://yuxile.54xuebaxue.com/index/Typepublic/tList',
+            header: {
+                'content-type': 'application/x-www-form-urlencoded'
+            },
+            method: 'POST',
+            success:function(res){
 
-  },
-  getteaching:function(){
-    var that = this
-    var user = wx.getStorageSync("userInfo")
-    console.log(gradeid)
-    wx.request({
-      url: 'http://yuxile.54xuebaxue.com/index/Textbook/tList',
-      header: {
-        'content-type': 'application/x-www-form-urlencoded'
-      },
-      method: 'POST',
-      data:{
-        course_id: courseid,
-        version_id: versionid,
-        grade_id: gradeid,
-        user_id: user.id
-      },
-      success:function(res){
-        console.log(res.data.data)
-        if (res.data.data.length < 1) {
-          num =  0/ 0 * 100
+                var list_wei=[]
 
-          that.setData({
-            arr: res.data.data,
-            num: num,
-            nianji: nianji,
-            banben: banben
-          })
-        }else{
-          num = res.data.data[0].read.num / res.data.data[0].read.all * 100
-          console.log(num)
-          that.setData({
-            arr: res.data.data,
-            num: num,
-            nianji: nianji,
-            banben: banben
-          })
+                console.log(res.data.data)
+                //科目ID
+                courseid = res.data.data.course_list[0].id
+                //版本ID
+                versionid = res.data.data.version_list[0].id
+                //年级id
+                gradeid = res.data.data.grade_list[0].id
+                that.getteaching()
+                console.log(versionid)
+                nianji=res.data.data.grade_list[0].name
+                banben = res.data.data.version_list[0].name
+                that.setData({
+                    class_list: res.data.data.grade_list,
+                    version_list: res.data.data.version_list,
+                    Subject_list: res.data.data.course_list,
+                    nianji: res.data.data.grade_list[0].name,
+                    banben: res.data.data.version_list[0].name
+
+                })
+
+            }
+        })
+    },
+    tobooklist:function(e){
+        var postad = e.currentTarget.dataset.id
+        //教材名称
+        wx.setStorageSync("jcmc", e.currentTarget.dataset.name)
+        console.log(postad)
+        wx.navigateTo({
+            //url: 'post-detail/post-detail'  //跳转详情页  切记配置app.json文件
+            url: '../booklist/booklist?id=' + postad + "&nianji=" + nianji + "&banben=" + banben  //传递参数
+        })
+
+    },
+    getteaching:function(){
+        var that = this
+        var user = wx.getStorageSync("userInfo")
+        console.log(gradeid)
+        wx.request({
+            url: 'http://yuxile.54xuebaxue.com/index/Textbook/tList',
+            header: {
+                'content-type': 'application/x-www-form-urlencoded'
+            },
+            method: 'POST',
+            data:{
+                course_id: courseid,
+                version_id: versionid,
+                grade_id: gradeid,
+                user_id: user.id
+            },
+            success:function(res){
+                console.log(res.data.data)
+                if (res.data.data.length < 1) {
+                    num =  0/ 0 * 100
+
+                    that.setData({
+                        arr: res.data.data,
+                        num: num,
+                        nianji: nianji,
+                        banben: banben
+                    })
+                }else{
+                    num = res.data.data[0].read.num / res.data.data[0].read.all * 100
+                    console.log(num)
+                    that.setData({
+                        arr: res.data.data,
+                        num: num,
+                        nianji: nianji,
+                        banben: banben
+                    })
+                }
+
+
+            }
+        })
+    },
+    onClick:function(){
+        console.log(courseid)
+        console.log(versionid)
+        console.log(gradeid)
+        this.getteaching()
+    },
+
+    classbtn:function(e){
+        var that = this
+        gradeid = e.currentTarget.dataset.id
+        nianji = e.currentTarget.dataset.name
+        console.log(nianji)
+        that.setData({
+            current01: e.currentTarget.dataset.index
+        })
+
+    },
+    typebtn: function (e) {
+        var that = this
+        versionid = e.currentTarget.dataset.id
+        banben = e.currentTarget.dataset.name
+
+        that.setData({
+            current02: e.currentTarget.dataset.index
+        })
+
+    },
+    yearbtn: function (e) {
+        var that = this
+        courseid = e.currentTarget.dataset.id
+        that.setData({
+            current03: e.currentTarget.dataset.index
+        })
+
+    },
+
+
+    toggle(type) {
+        this.setData({
+            [`show.${type}`]: !this.data.show[type]
+        });
+    },
+
+    toggleRightPopup() {
+        this.toggle('right');
+    },
+
+    filter: function () {
+        this.toggle('right');
+    },
+    swiperChange: function (e) {
+        console.log(e.detail);
+        if (e.detail.source == "touch"){
+            let swiperError = this.data.swiperError
+            swiperError += 1
+            this.setData({ swiperError: swiperError })
+            if (swiperError >= 3) { //在开关被触发3次以上
+                console.error(this.data.swiperError)
+                this.setData({ swiperCurrent: this.data.swiperCurrent });//，重置current为正确索引
+                this.setData({ swiperError: 0 })
+            }else{
+                this.setData({
+                    swiperCurrent: e.detail.current  //获取当前轮播图片的下标
+                })
+            }
         }
-       
-       
-      }
-    })
-  },
-  onClick:function(){
-    console.log(courseid)
-    console.log(versionid)
-    console.log(gradeid)
-    this.getteaching()
-  },
+    },
+    //滑动图片切换
+    chuangEvent: function (e) {
+        this.setData({
+            swiperCurrent: e.currentTarget.id
+        })
+    },
 
-  classbtn:function(e){
-    var that = this
-    gradeid = e.currentTarget.dataset.id
-    nianji = e.currentTarget.dataset.name
-    console.log(nianji)
-    that.setData({
-      current01: e.currentTarget.dataset.index
-    })
-   
-  },
-  typebtn: function (e) {
-    var that = this
-    versionid = e.currentTarget.dataset.id
-    banben = e.currentTarget.dataset.name
-    
-    that.setData({
-      current02: e.currentTarget.dataset.index
-    })
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        this.getdata()
 
-  },
-  yearbtn: function (e) {
-    var that = this
-    courseid = e.currentTarget.dataset.id
-    that.setData({
-      current03: e.currentTarget.dataset.index
-    })
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+        this.animation = wx.createAnimation()
 
+    },
 
-  toggle(type) {
-    this.setData({
-      [`show.${type}`]: !this.data.show[type]
-    });
-  },
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+        var that = this;
+        that.component = that.selectComponent("#component")
+        that.component.customMethod()
+    },
 
-  toggleRightPopup() {
-    this.toggle('right');
-  },
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+        var that = this;
+        that.component = that.selectComponent("#component")
+        that.component.noShow()
+        that.component.nohide()
+    },
 
-  filter: function () {
-    this.toggle('right');
-  },
-  swiperChange: function (e) {
-    console.log(e.detail);
-    if (e.detail.source == "touch"){
-      let swiperError = this.data.swiperError
-      swiperError += 1
-      this.setData({ swiperError: swiperError })
-      if (swiperError >= 3) { //在开关被触发3次以上
-        console.error(this.data.swiperError)
-        this.setData({ swiperCurrent: this.data.swiperCurrent });//，重置current为正确索引
-        this.setData({ swiperError: 0 })
-    }else{
-      this.setData({
-        swiperCurrent: e.detail.current  //获取当前轮播图片的下标
-      })
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
     }
-    }
-  },
-  //滑动图片切换 
-  chuangEvent: function (e) {
-    this.setData({
-      swiperCurrent: e.currentTarget.id
-    })
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    this.getdata()
-   
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    this.animation = wx.createAnimation()
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    var that = this;
-    that.component = that.selectComponent("#component")
-    that.component.customMethod()
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    var that = this;
-    that.component = that.selectComponent("#component")
-    that.component.noShow()
-    that.component.nohide()
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
--- pages/public/public.js
+++ pages/public/public.js
@@ -2,207 +2,213 @@ var ss = ""
 var list = []
 var flg=true
 var flgging = true
+const app = getApp();
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-      flg:true,
-    flgging: true
-  },
+    /**
+     * 页面的初始数据
+     */
+    "data": {
+        flg:true,
+        flgging: true
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
 
 
-  },
- 
+    },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
 
-  },
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+    },
 
-  },
+    /**
+     *生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+    },
 
-  },
+    /**
+     *页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+    },
 
-  },
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+    },
 
-  }
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
 })
 
 Component({
-  properties: {
-    // 这里定义了innerText属性，属性值可以在组件使用时指定
+    properties: {
+        // ?????innerText????????????????
 
-  },
-  data: {
-    // 这里是一些组件内部数据
-    someData: {}
-  },
-  methods: {
-    // 这里是一个自定义方法
-    customMethod: function () {
-      var that = this
-      wx.connectSocket({
-        // // url: 'ws://121.40.165.18:8800',
-        // url: 'ws://192.168.0.129:8282',
-        success: function () {
-          console.log("连接成功")
-        },
-        fail: function () {
-          console.log("连接失败")
-        }
-
-      })
-      wx.onSocketOpen(function () {
-        console.log("已打开")
-        wx.sendSocketMessage({
-          data: 'stock',
-
-        })
-      })
-      wx.onSocketMessage(function (data) {
-        console.log(1)
-
-        ss = data.data
-        list.push(data.data)
-
-        var animation = wx.createAnimation({
-          // 动画持续时间
-          duration: 500,
-          // 定义动画效果，当前是匀速
-          timingFunction: 'linear'
-        })
-        that.animation = animation
-        animation.translateY(550).step()
-
-        that.setData({
-          // 通过export()方法导出数据
-          animationData: animation.export(),
-          // 改变view里面的Wx：if
-          modelFlag: true
-        })
-        // 设置setTimeout来改变y轴偏移量，实现有感觉的滑动
-        setTimeout(function () {
-          animation.translateY(0).step()
-          that.setData({
-            animationData: animation.export()
-          })
-        }, 200)
+    },
+    data: {
+        // ???????????
+        someData: {}
+    },
+    methods: {
         
+        customMethod: function () {
+            var that = this
+            console.log(app.userInfo.id)
+            wx.connectSocket({
+                // url: 'ws://121.40.165.18:8800',
+                // url: 'ws://192.168.0.129:8282',
+                success: function () {
+                    console.log("连接成功")
+                },
+                fail: function () {
+                    console.log("连接失败")
+                }
+
+            })
+            wx.onSocketOpen(function () {
+                console.log("打开连接")
+                wx.sendSocketMessage({
+                    data: app.userInfo.id,
+                    success:function(res){
+                        console.log(res)
+                    }
+
+                })
+            })
+
+            wx.onSocketMessage(function (data) {
+                console.log(1)
+
+                ss = data.data
+                list.push(data.data)
+
+                var animation = wx.createAnimation({
+                   
+                    duration: 500,
+                   
+                    timingFunction: 'linear'
+                })
+                that.animation = animation
+                animation.translateY(550).step()
+
+                that.setData({
+                  
+                    animationData: animation.export(),
+                    
+                    modelFlag: true
+                })
+               
+                setTimeout(function () {
+                    animation.translateY(0).step()
+                    that.setData({
+                        animationData: animation.export()
+                    })
+                }, 200)
 
 
-        that.setData({
-          list_sun: list,
-          flg:true,
-          flgging: true
-        })
-        list = []
 
-      })
-    },
-    noShow: function () {
-      var that = this;
-      var animation = wx.createAnimation({
-        duration: 1000,
-        timingFunction: 'linear'
-      })
-      that.animation = animation
-      animation.translateY(550).step()
-      that.setData({
-        animationData: animation.export()
+                that.setData({
+                    list_sun: list,
+                    flg:true,
+                    flgging: true
+                })
+                list = []
 
-      })
-      setTimeout(function () {
-        animation.translateY(0).step()
-        that.setData({
-          animationData: animation.export(),
-          modelFlag: false
-        })
-      }, 200)
-    },
-    close: function () {
-      var that = this
-      console.log(111)
-      if (flg == true) {
-        flg = false;
-        that.setData({
-          flg: false
-        })
+            })
+        },
+        noShow: function () {
+            var that = this;
+            var animation = wx.createAnimation({
+                duration: 1000,
+                timingFunction: 'linear'
+            })
+            that.animation = animation
+            animation.translateY(550).step()
+            that.setData({
+                animationData: animation.export()
+
+            })
+            setTimeout(function () {
+                animation.translateY(0).step()
+                that.setData({
+                    animationData: animation.export(),
+                    modelFlag: false
+                })
+            }, 200)
+        },
+        close: function () {
+            var that = this
+            console.log(111)
+            if (flg == true) {
+                flg = false;
+                that.setData({
+                    flg: false
+                })
 
 
-      } else {
-        flg = true;
-        that.setData({
-          flg: true
-        })
-      }
-    },
-    type02: function () {
-      var that = this
-      console.log(111)
-      if (flgging == true) {
-        flgging = false;
-        that.setData({
-          flgging: false
-        })
-      } else {
-        flgging = true;
-        that.setData({
-          flgging: true
-        })
-      }
-    },
-    nohide:function(){
-      flgging = true;
-      flg = true;
-      this.setData({
-        flgging: false,
-        flg: false
-      })
+            } else {
+                flg = true;
+                that.setData({
+                    flg: true
+                })
+            }
+        },
+        type02: function () {
+            var that = this
+            console.log(111)
+            if (flgging == true) {
+                flgging = false;
+                that.setData({
+                    flgging: false
+                })
+            } else {
+                flgging = true;
+                that.setData({
+                    flgging: true
+                })
+            }
+        },
+        nohide:function(){
+            flgging = true;
+            flg = true;
+            this.setData({
+                flgging: false,
+                flg: false
+            })
+        }
     }
-  }
 })
 
 
