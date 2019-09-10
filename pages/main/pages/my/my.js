9e2eb09e15ad6209c448827bfa00231a66916d7d
Administrator <weixiangyingln@163.com>
2019-09-10 15:49:33 +0800

    我的页面添加收益

--- pages/main/pages/my/my.js
+++ pages/main/pages/my/my.js
@@ -9,11 +9,13 @@ Page({
         msgCount: 0,
         showTab: true,
         my_study:"",
-      my_daily: ""
+      my_daily: "",
+      RenInfo:"",
     },
     onLoad(){
       var that = this
       that.getshow()
+      that.getRenInfo();
         this.compontNavbar = this.selectComponent("#compontNavbar");
       if (app.userInfo.mobile == '18647993992') {
         this.setData({
@@ -21,25 +23,7 @@ Page({
         })
       }  
     },
-  getshow: function () {
-    var that = this
-    wx.request({
-      url: app.requestUrl + 'v14/public/display',
-      data: {},
-      method: "POST",
-      header: {
-        "Content-Type": "application/x-www-form-urlencoded"
-      },
-      success: function (res) {
-        console.log(res.data.data[0].list[0].my_daily)
-        that.setData({
-          my_study: res.data.data[0].list[0].my_study,
-          my_daily: res.data.data[0].list[0].my_daily,
 
-        })
-      }
-    })
-  },
   onHide: function () {
     var that = this;
     that.component = that.selectComponent("#component")
@@ -151,5 +135,40 @@ Page({
         wx.navigateTo({
             url: '/pages/common/webView/webView?src=' + app.ansHref + '&uid=' + app.userInfo.id + '&miniPro=1'
         });
-    }
+    },
+  getRenInfo() {
+    var that = this;
+    network.POST({
+      url: 'v14/renewal/index',
+      params: {
+        "mobile": app.userInfo.mobile,
+        "token": app.userInfo.token,
+        "order_type": 1,
+        // "order_type_id": 0,
+      },
+      success: function (res) {
+        console.log(res);
+        wx.hideLoading();
+        if (res.data.code == 200) {
+          that.setData({
+            renInfo: res.data.data[0].item
+          });
+        } else {
+          wx.showToast({
+            title: res.data.message,
+            icon: 'none',
+            duration: 1000
+          })
+        }
+      },
+      fail: function () {
+        wx.hideLoading();
+        wx.showToast({
+          title: '服务器异常',
+          icon: 'none',
+          duration: 1000
+        })
+      }
+    });
+  },
 })
